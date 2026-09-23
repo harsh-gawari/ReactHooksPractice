2dc7d7117cdb0c2a52dca3ada793732bcb215c61
Harsh Gawari <harshgawari42@gmail.com>
2026-09-23 22:42:09 +0530

    Practice example of useeffect  and usestate hook

--- hooksPractice/src/components/UseeffectPractice.jsx
+++ hooksPractice/src/components/UseeffectPractice.jsx
@@ -1,21 +1,43 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 function UseeffectPractice() {
 
     // what is useEffect hook
     // useeffect is hook  which are used to sideeffect means used to side Api call
     // mostly used to call backend api and fetch the data from backend server and display in frontend
-    useEffect(() => {
-        // console.log("hello world");
-        axios.get("http//localhost:8080/setcar")
-            .then((res) => { res.data })
-            .catch(error => {
-                console.log("error");
+    // useEffect(() => {
+    //     // console.log("hello world");
+    //     axios.get("http//localhost:8080/setcar")
+    //         .then((res) => { res.data })
+    //         .catch(error => {
+    //             console.log("error");
 
-            })
-    }, [])
+    //         })
+    // }, [])
+
+
+    //     Create a component that:
+
+    // Has a count state starting at 0.
+    // Has a button Increment.
+    // Every time count changes, print this in the console:
+
+    const [count, setCount] = useState(0);
+
+    useEffect(() => {
+        console.log("count changed :", count);
+    })
+
     return (
-        <div>UseeffectPractice</div>
+        <>
+            <div>
+                <h1>Count: {count}</h1>
+
+                <button onClick={() => setCount(count + 1)}>
+                    Increment
+                </button>
+            </div>
+        </>
     )
 }
 
